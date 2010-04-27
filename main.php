01de685b6cacc803bc692acbb9361d76494db409
Taha Goulamhoussen <taha.goulamhoussen@gmail.com>
2010-04-27 18:28:47 +0200

    add comment: storageEngine and storageLocation relevant

--- main.php
+++ main.php
@@ -3,6 +3,7 @@ require_once "Classes/FusionLib.class.php";
 
 $fusionLib = FusionLib::getInstance();
 
+// storageEngine and storageLocation relevant
 $myConfigs = array(storageEngine => "directory", storageLocation => "/data", applicationName => "GLPI", criterias => array("asset tag", "motherboard serial"));
 
 $fusionLib->setConfigs($myConfigs);

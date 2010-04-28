c472d384bdec19e34bc5202ef471db4dbba03c95
Taha Goulamhoussen <taha.goulamhoussen@gmail.com>
2010-04-28 11:27:17 +0200

    criterias array changed

--- Classes/FusionLib.class.php
+++ Classes/FusionLib.class.php
@@ -5,171 +5,196 @@ require_once "hooks.class.php";
 
 class FusionLib
 {
-	
-	protected static $_instance;
-	
-	private $_configs;
-	
-	private $_machine;
-	private $_section;
-	
-	/**
-     * Disable instance
-	 * @access private
-     */
-	private function __construct()
-	{
-	}
-	
-	
-	/**
-     * Singleton
-     */
-	public static function getInstance()
-    {
-		if(self::$_instance == null) {
-            self::$_instance = new self();
-        }
-        return self::$_instance;
-		
+  
+  protected static $_instance;
+  
+  private $_configs;
+  
+  private $_machine;
+  private $_section;
+  
+  /**
+  * Disable instance
+  * @access private
+  */
+  private function __construct()
+  {
+  }
+  
+  
+  /**
+  * Singleton
+  */
+  public static function getInstance()
+  {
+    if(self::$_instance == null) {
+      self::$_instance = new self();
     }
-	
-	/**
-     * Initialization
-     */
-	private function _init()
-    {
-		$this->_machine = new Machine();
-		$this->_section = new Section();		
+    return self::$_instance;
+    
+  }
+  
+  /**
+  * Initialization
+  */
+  private function _init()
+  {
+    $this->_machine = new Machine();
+    $this->_section = new Section();		
+  }
+  
+  /**
+  * Configs :
+  * User defines:
+  * - where and how the data will be store
+  * - the application that will use the library
+  * - the priority and the list of criterias
+  * @param array $configs (
+  storageEngine => "directory", 
+  storageLocation => "/data", 
+  applicationName => "GLPI", 
+  criterias => array(maxFalse => 2, items => array("asset tag", "motherboard serial")))
+  )
+  */
+  public function setConfigs($configs)
+  {
+    if(isset($configs["storageEngine"], $configs["storageLocation"], $configs["applicationName"], $configs["criterias"])){
+      
+      if (!(in_array($configs["storageEngine"], array("directory", "database"))))
+      {
+	throw new Exception ("storageEngine that you specified doesn't exist");
+      }
+      
+      if (!(is_string($configs["storageLocation"])))
+      {
+	throw new Exception ("storageLocation isn't a string");
+      }
+      
+      if (!(is_string($configs["applicationName"])))
+      {
+	throw new Exception ("applicationName isn't a string");
+      }
+      
+      $definedCriterias = array(
+      "motherboardSerial",
+      "assetTag",
+      "msn",
+      "ssn",
+      "baseboardSerial",
+      "macAddress",
+      "uuid",
+      "winProdKey",
+      "biosSerial",
+      "enclosureSerial",
+      "smodel",
+      "storagesSerial",
+      "drivesSerial")
+      
+      foreach($configs["criterias"]["items"] as $criteria)
+      {
+	if (!(in_array($criteria, $definedCriterias)))
+	{
+	  throw new Exception ("an criteria that you specified doesn't exist");
+	}
+      }
+      
+      $this->_configs = $configs;
+      
+    } else {
+      throw new Exception ("you have to complete correctly configuration array");
     }
     
-	/**
-	* Configs :
-	* User defines:
-	* - where and how the data will be store
-	* - the application that will use the library
-	* - the priority and the list of criterias
-	* @param array $configs (storageEngine => "directory", storageLocation => "/data", applicationName => "GLPI", criterias => array("asset tag", "motherboard serial"))
-)
-	*/
-	public function setConfigs($configs)
-    {
-        if(isset($configs["storageEngine"], $configs["storageLocation"], $configs["applicationName"], $configs["criterias"])){
-			
-			if (!(in_array($configs["storageEngine"], array("directory", "database"))))
-			{
-				throw new Exception ("storageEngine that you specified doesn't exist");
-			}
-			
-			if (!(is_string($configs["storageLocation"])))
-			{
-				throw new Exception ("storageLocation isn't a string");
-			}
-			
-			if (!(is_string($configs["applicationName"])))
-			{
-				throw new Exception ("applicationName isn't a string");
-			}
-			
-			foreach($configs["criterias"] as $criteria)
-			{
-				if (!(in_array($criteria, array("asset tag", "motherboard serial"))))
-				{
-					throw new Exception ("an criteria that you specified doesn't exist");
-				}
-			}
-			
-			$this->_configs = $configs;
-			
-		} else {
-			throw new Exception ("you have to complete correctly configuration array");
-		}
-		
-
+    
+  }
+  
+  public function start()
+  {
+    $this->_init();
+    // TODO: file reception
+    try {
+      $req = simplexml_load_file("data/aofr.ocs");
+    } catch (Exception $e){
+      echo 'Exception : ',  $e->getMessage(), "\n";
     }
-	
-	public function start()
+    
+    if ($this->_isMachineExist($req))
     {
-        $this->_init();
-		
-		// TODO: file reception
-		try {
-		    $req = simplexml_load_file("data/aofr.ocs");
-		} catch (Exception $e){
-			echo 'Exception : ',  $e->getMessage(), "\n";
-		}
-		
-		if ($this->_isMachineExist($req))
-		{
-			
-			echo " machine exists";
-			
-			
-		} else {
-			
-			echo " machine doesn't exist";
-			
-			//We launch CreateMachine() hook and provide an InternalId (how?)
-			
-			try {
-			    $internalId = 12; // TODO
-			    $externalId = Hooks::CreateMachine();			
-			    $this->_addLibMachine($internalId, $externalId);
-			} catch (Exception $e){
-				
-			}
-		}
-
+      
+      echo " machine exists";
+      
+      
+    } else {
+      
+      echo " machine doesn't exist";
+      
+      //We launch CreateMachine() hook and provide an InternalId (how?)
+      
+      try {
+	$internalId = 12; // TODO
+	$externalId = Hooks::CreateMachine();			
+	$this->_addLibMachine($internalId, $externalId);
+      } catch (Exception $e){
+	
+      }
     }
-	
-	
-	/**
-   * We look for the machine with the relevant criterias defined by user, if it doesn't exist, return false; else return true.
-   * @param $req SimpleXml object
-   * @return $bool boolean
-   */
-	private function _isMachineExist($simpleXMLObj)
-	{
-		foreach($this->_configs["criterias"] as $criteria)
-		{
-			switch($criteria){
-				case "asset tag":
-				if ($simpleXMLObj->CONTENT->BIOS->ASSETTAG)
-				{
-					if (file_exists($configs["storageLocation"]."/"."assettag"."/".$configs["applicationName"]."/".$simpleXMLObj->CONTENT->BIOS->ASSETTAG))
-					{
-						continue;
-					} else {
-						return false;
-					}
-				}
-				break;
-				case "motherboard serial":
-				if ($simpleXMLObj->CONTENT->BIOS->ASSETTA)
-				{
-					var_dump($simpleXMLObj->CONTENT->BIOS->ASSETTAG);
-				}
-				break;
-				
-				
-			}
-		}
-		
-		return true;
-	}
-	
-	/**
-   * We create directory tree for machine and store software name and the externalId within YAML file.
-   * @param $internalId
-   * @param $externalId
-   */
-	private function _addLibMachine($internalId, $externalId)
-	{
-		
-	
-	}
-	
-
+    
+  }
+  
+  
+  /**
+  * We look for the machine with the relevant criterias defined by user, if it doesn't exist, return false; else return true.
+  * @param $req SimpleXml object
+  * @return $bool boolean
+  */
+  private function _isMachineExist($simpleXMLObj)
+  {
+    foreach($this->_configs["criterias"] as $criteria)
+    {
+      switch($criteria){
+	case "asset tag":
+	  if ($simpleXMLObj->CONTENT->BIOS->ASSETTAG)
+	  {
+	    if (file_exists($configs["storageLocation"]."/"."assettag"."/".$configs["applicationName"]."/".$simpleXMLObj->CONTENT->BIOS->ASSETTAG))
+	    {
+	      continue;
+	    } else {
+	      return false;
+	    }
+	  }
+	  break;
+	case "motherboardSerial":
+	  if ($simpleXMLObj->CONTENT->BIOS->ASSETTA)
+	  {
+	    var_dump($simpleXMLObj->CONTENT->BIOS->ASSETTAG);
+	  }
+	  break;
+	  
+	  
+      }
+    }
+    
+    return true;
+  }
+  
+  /**
+  * We create directory tree for machine and store software name and the externalId within YAML file.
+  * @param $internalId
+  * @param $externalId
+  */
+  private function _addLibMachine($internalId, $externalId)
+  {
+    
+    
+  }
+  
+  private function _getCriteriaDSN()
+  {
+    
+    
+    
+  }
+  
+  
 }
 
 ?>
--- main.php
+++ main.php
@@ -4,7 +4,11 @@ require_once "Classes/FusionLib.class.php";
 $fusionLib = FusionLib::getInstance();
 
 // storageEngine and storageLocation relevant
-$myConfigs = array(storageEngine => "directory", storageLocation => "/data", applicationName => "GLPI", criterias => array("asset tag", "motherboard serial"));
+$myConfigs = array(
+storageEngine => "directory", 
+storageLocation => "/data", 
+applicationName => "GLPI", 
+criterias => array(maxFalse => 2, items => array("asset tag", "motherboard serial")));
 
 $fusionLib->setConfigs($myConfigs);
 
